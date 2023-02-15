74e8abadfd918f75bf1809800dd2e74ea40ee4b7
globalshot <31039763+globalshot@users.noreply.github.com>
2023-02-15 16:44:32 +0200

    phase 1 done

--- js/meme-controller.js
+++ js/meme-controller.js
@@ -8,6 +8,9 @@ var gCtxText = gElCanvasText.getContext('2d')
 var font = 'sans-serif'
 // renderMeme()
 function renderMeme() {
+    // newImgSelected(id)
+    // toggleGallery()
+    gCtxImg.clearRect(0, 0, gElCanvasImg.width, gElCanvasImg.height);
     let img = new Image()
     // gCtx.beginPath()
     img.addEventListener('load', (event) => {gCtxImg.drawImage(img, 0, 0, gElCanvasImg.width, gElCanvasImg.height)})
@@ -34,19 +37,14 @@ function renderMeme() {
     document.getElementById('text-line').addEventListener('keyup', function () {
         gCtxText.clearRect(0, 0, gElCanvasText.width, gElCanvasText.height);
         setLineTxt(document.getElementById('text-line').value)
-        console.log(document.getElementById('text-line').value);
-        // gCtxText.fillStyle = '#7fffd4';
-        // gCtxText.font = '10px sans-serif';//change ofc this
-        gCtxText.font = `${getLineSize()}px ${font}`;//change ofc this
+        // console.log(document.getElementById('text-line').value);
         
         //should i just return gMeme.lines or no, mmmm
         var stringTitle = getLineTxt()
         gCtxText.fillStyle = getLineColor()  //text color
-        // gCtxText.size = getLineSize() //size
-        console.log('getLineSize()',getLineSize());
         // gCtxText  //alignment
+        
         // gCtxText  //font?
-        
-        
-        // var text_title = stringTitle;
+        gCtxText.font = `${getLineSize()}px ${font}`;//change ofc this
+        //change the position otherwise it reset each time, i guess
         gCtxText.fillText(stringTitle, gElCanvasText.width/3, 35)}, 100)
--- js/services/meme-service.js
+++ js/services/meme-service.js
@@ -1,7 +1,26 @@
 
 var gKeywordSearchCountMap = {'funny': 12,'cat': 16, 'baby': 2} 
  
-var gImgs = [{id: 1, url: 'img/1.jpg', keywords: ['funny', 'cat']}]; 
+var gImgs = [
+    {id: 1, url: 'img/1.jpg', keywords: ['trump']},
+    {id: 2, url: 'img/2.jpg', keywords: ['love', 'dog']},
+    {id: 3, url: 'img/3.jpg', keywords: ['funny', 'cat']},
+    {id: 4, url: 'img/4.jpg', keywords: ['funny', 'cat']},
+    {id: 5, url: 'img/5.jpg', keywords: ['funny', 'cat']},
+    {id: 6, url: 'img/6.jpg', keywords: ['funny', 'cat']},
+    {id: 7, url: 'img/7.jpg', keywords: ['funny', 'cat']},
+    {id: 8, url: 'img/8.jpg', keywords: ['funny', 'cat']},
+    {id: 9, url: 'img/9.jpg', keywords: ['funny', 'cat']},
+    {id: 10, url: 'img/10.jpg', keywords: ['funny', 'cat']},
+    {id: 11, url: 'img/11.jpg', keywords: ['funny', 'cat']},
+    {id: 12, url: 'img/12.jpg', keywords: ['funny', 'cat']},
+    {id: 13, url: 'img/13.jpg', keywords: ['funny', 'cat']},
+    {id: 14, url: 'img/14.jpg', keywords: ['funny', 'cat']},
+    {id: 15, url: 'img/15.jpg', keywords: ['funny', 'cat']},
+    {id: 16, url: 'img/16.jpg', keywords: ['funny', 'cat']},
+    {id: 17, url: 'img/17.jpg', keywords: ['funny', 'cat']},
+    {id: 18, url: 'img/18.jpg', keywords: ['funny', 'cat']},
+]; 
 var gMeme = { 
     selectedImgId: 1, //can be updated without the
     selectedLineIdx: 0, 
@@ -15,8 +34,13 @@ var gMeme = {
     ] 
 }
 
+function getImages() {
+    return gImgs.slice()
+}
+
+
 function getCurrImg() {
-    return  `meme-imgs (square)/${gMeme.selectedImgId}.jpg`
+    return  `img/${gMeme.selectedImgId}.jpg`
     // return gMeme.selectedImgId
 }
 
@@ -35,4 +59,11 @@ function getLineColor() {
 
 function getLineSize() {
     return gMeme.lines[gMeme.selectedLineIdx].size
+}
+
+function onImgSelect(id) {
+    gMeme.selectedImgId = id
+    gMeme.selectedLineIdx = 0
+    renderMeme()
+    toggleGallery()
 }
